8d446525d2d9a8985a093cf809af70ab610de8ab
Dmitriy Esko <dmitriy.esko@gmail.com>
2021-01-09 09:31:13 +0300

    basic styling

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import classNames from 'classnames'
 // import flagIcon from '...'
 
 const Tile = ( { data, setGameOver, openTile, setFlag } ) => {
 
   return(
     <div
-      className='tile'
       onClick={ () => {
         if (!data.isFlag) {
           openTile(data.index)
@@ -21,10 +21,28 @@ const Tile = ( { data, setGameOver, openTile, setFlag } ) => {
           setFlag(data.index)
         }
       }}
+      className='tile'
     >
-      {data.isFlag && 'F'}
-      {data.isOpen && !data.isBomb && data.number}
-      {data.isOpen && data.isBomb && 'B'}
+      <div className={
+        classNames({
+          'tile__open': data.isOpen,
+          'tile__closed': !data.isOpen || data.isFlag,
+          'color__one': !data.isFlag && !data.isBomb && data.number === 1,
+          'color__two': !data.isFlag && !data.isBomb && data.number === 2,
+          'color__three': !data.isFlag && !data.isBomb && data.number === 3,
+          'color__four': !data.isFlag && !data.isBomb && data.number === 4,
+          'color__five': !data.isFlag && !data.isBomb && data.number === 5,
+          'color__six': !data.isFlag && !data.isBomb && data.number === 6,
+          'color__seven': !data.isFlag && !data.isBomb && data.number === 7,
+          'color__eight': !data.isFlag && !data.isBomb && data.number === 8,
+          'bomb': data.isOpen && data.isBomb,
+          'flag': data.isFlag,
+        })
+      }>
+        {data.isFlag && 'F'}
+        {data.isOpen && !data.isBomb && data.number !== 0 && data.number}
+        {data.isOpen && data.isBomb && 'B'}
+      </div>
     </div>
   )
 }
